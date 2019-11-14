e9b1100b446ad0a3b5fe0e6f76b9e3c8a8f1cc7a
unknown <piece@csf.org.tw>
2019-11-14 17:55:24 +0800

    First push

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# CSFAI
+CSF AI solution
+======
+
+介紹CSF在AI及運算思維的相關產品項目：
+
+1. AI相關認證
+    1. TQC+ 程式語言 Python
+    2. TQC+ 網頁資料擷取與分析 Python
+    3. TQC 人工智慧應用與技術
+2. 運算思維相關認證
+    1. TQC+ App Inventor程式設計核心能力
+    2. TQC+ 基礎創意App程式設計
+    3. TQC+ 創意App程式設計
+3. Code Judger平台
+4. 雲端練功坊
+5. 相關出版品
+    1. TQC+ Python 3.x 程式語言特訓教材
+    2. TQC+ 網頁資料擷取與分析 Python
+    3. TQC 人工智慧應用與技術實力養成暨評量
+    4. TQC+ 創意App程式設計認證指南 App Inventor 2
+    5. TQC+ 創意App程式設計特訓教材 App Inventor 2
+    6. TQC+ 創意App程式設計認證指南解題秘笈-App Inventor 2
---
+++ page/js/index.js
@@ -0,0 +1,34 @@
+(function ($) {
+    'use strict';
+    $(".btn.sonar-btn.white-btn").click(function(){
+        $(".read-more").toggle(1000);
+    });
+
+
+    // $(".btn.sonar-btn").click(function(){      
+    //     $(".load-more-box").css("height","550px");
+            
+    //     var $grid = $('.sonar-portfolio').isotope({
+    //         itemSelector: '.single_gallery_item',
+    //         percentPosition: true,
+    //         masonry: {
+    //             columnWidth: '.single_gallery_item'
+    //         }
+    //     });
+        
+    //     $(".load-more").animate({
+    //         opacity: "1"
+    //     },1000);
+        
+    // });
+
+  
+
+
+
+
+
+    
+})(jQuery);
+
+
